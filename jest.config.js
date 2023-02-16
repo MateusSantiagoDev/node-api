ad8c21cde63036dcf635a641f89f45af62e09131
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-16 15:31:58 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts', // quero coletar tudo de src
+    '!<rootDir>/src/main/**' // porem não quero commitar o diretório /main
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  /*  preset: '@shelf/jest-mongodb', */
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
