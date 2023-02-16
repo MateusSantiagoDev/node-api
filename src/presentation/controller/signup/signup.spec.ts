7a3fc89811d274c8c8c4afbc2989e03bf5ee6539
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-16 18:31:08 -0300

    test: ensure return 400 if password confirmation fails

--- src/presentation/controller/signup/signup.spec.ts
+++ src/presentation/controller/signup/signup.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParam } from '../../error/invalid-param-error'
 import { MissingParam } from '../../error/missing-param-error'
 import { SignUpController } from './signup'
 
@@ -60,5 +61,20 @@ describe('SignUp Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParam('confirmPassword'))
+  })
+
+  test('Should return 400 if password confirmation fails', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        confirmPassword: 'invalid_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParam('invalid_password'))
   })
 })
