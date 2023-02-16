088fd12039658d05eb7b252306a67eb2fd872680
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-16 16:26:39 -0300

    test: ensure return 400 if no name, email, password, confirmPasword is provided

---
+++ src/presentation/controller/signup/signup.spec.ts
@@ -0,0 +1,64 @@
+import { MissingParam } from '../../error/missing-param-error'
+import { SignUpController } from './signup'
+
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        confirmPassword: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParam('name'))
+  })
+
+  test('Should return 400 if no email is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        confirmPassword: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParam('email'))
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        confirmPassword: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParam('password'))
+  })
+
+  test('Should return 400 if no confirmPassword is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParam('confirmPassword'))
+  })
+})
