2527c71ad55e55adc6361fc4bf9cbb7cca4e6acb
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-16 21:20:04 -0300

    test: ensure return true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -18,4 +18,10 @@ describe('EmailValidator Adapter', () => {
     const inValid = sut.isValid('invalid_email@mail.com')
     expect(inValid).toBe(false)
   })
+
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
 })
