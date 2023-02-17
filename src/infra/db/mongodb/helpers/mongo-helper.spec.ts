af0271488ad6ad2941ebbee830a868230f3fb9bc
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-17 16:41:04 -0300

    test: ensure mongodb recconect if client is down

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,22 @@
+import { MongoHelper as sut } from './mongo-helper'
+// importando assim por não ser uma class
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  // teste para garantir que se o mongodb perder a conexão
+  // ele vai ser reconectado
+  test('Should recconect if mongodb is down', async () => {
+    let accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy() // espero q ele exista
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
