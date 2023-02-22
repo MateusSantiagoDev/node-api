66b333791ef88e7f5158a3e0f3b740a6b13758b9
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-22 18:20:34 -0300

    refactor: move data layes interfaces to folders

--- src/data/usecase/add-account/db-add-account.spec.ts
+++ src/data/usecase/add-account/db-add-account.spec.ts
@@ -1,7 +1,7 @@
 import { AccountModel } from '../../../domain/model/account'
 import { AccountDto, AddAccount } from '../../../domain/usecase/add-account'
-import { AddAccountRepository } from '../../protocols/add-account-repository'
-import { Encrypter } from '../../protocols/encrypter'
+import { AddAccountRepository } from '../../protocols/db/add-account-repository'
+import { Encrypter } from '../../protocols/criptography/encrypter'
 import { DbAccount } from './db-add-account'
 
 const makeFakeAccount = (): AccountModel => ({
