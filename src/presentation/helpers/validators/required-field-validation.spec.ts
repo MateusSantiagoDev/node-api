dad82b3311c5b18a225e211ce39e9f46bd680d41
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-22 10:46:34 -0300

    test: ensure RequiredFieldVaidation not return if validation succeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -10,4 +10,14 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ name: 'invalid_param' })
     expect(error).toEqual(new MissingParam('field'))
   })
+
+  // se não houver falha o teste vai garantir
+  // que não seja retornado nada
+  test('Should not return if validation succeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    // nesse caso o validate vai ser chamado com um field
+    const error = sut.validate({ field: 'any_param' })
+    // se ele tiver um field ele não vai retornar nada
+    expect(error).toBeFalsy()
+  })
 })
