6e35b9a559648cd9f4dc86f0321830c56f494ca9
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-22 11:31:51 -0300

    refactor: move sut cretion to a fectory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -5,21 +5,32 @@ import { Validation } from './validation'
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
-      return new MissingParam('field')
+      return null
     }
   }
   return new ValidationStub()
 }
 
-const makeSut = (): ValidationComposite => {
-  return new ValidationComposite([makeValidation()])
+interface sutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): sutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
 }
 
 describe('Validation Composite', () => {
   // se algum validation interno do composite falhar
   // esse teste vai retornar o mesmo erro que o validation retornou
   test('Should return an error if any validation fails', () => {
-    const sut = makeSut()
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParam('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParam('field'))
   })
