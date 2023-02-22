abaaf6444b146bc5467bf5affa03e4f4694d1c9c
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-22 10:49:01 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,11 +1,14 @@
 import { MissingParam } from '../../error'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
 describe('RequiredField Validation', () => {
   // teste para garantir que se a validação
   // falhar sera retornando um missingParam
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     // verificar se o campo "field" vai ser passado no validate
     const error = sut.validate({ name: 'invalid_param' })
     expect(error).toEqual(new MissingParam('field'))
@@ -14,7 +17,7 @@ describe('RequiredField Validation', () => {
   // se não houver falha o teste vai garantir
   // que não seja retornado nada
   test('Should not return if validation succeds', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     // nesse caso o validate vai ser chamado com um field
     const error = sut.validate({ field: 'any_param' })
     // se ele tiver um field ele não vai retornar nada
