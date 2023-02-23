77903852443603b3ce6ee80000e141e8e72f1470
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-23 13:06:44 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
-describe('SignUp Router', () => {
+describe('Login Router', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -15,16 +15,17 @@ describe('SignUp Router', () => {
     const collection = await MongoHelper.getCollection('accounts')
     await collection.deleteMany({})
   })
-
-  test('Should return an account on sucess', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Mateus',
-        email: 'mateus_test@email.com',
-        password: '1234',
-        confirmPassword: '1234'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Mateus',
+          email: 'mateus_test@email.com',
+          password: '1234',
+          confirmPassword: '1234'
+        })
+        .expect(200)
+    })
   })
 })
