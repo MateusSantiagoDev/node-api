ff50c0d621bb17a268776aecd89c17b9475e81d9
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-24 19:48:22 -0300

    feat: ensure DbAddAccount returns null if loadAccountByEmailRepository returns an account

--- src/data/usecase/add-account/db-add-account.ts
+++ src/data/usecase/add-account/db-add-account.ts
@@ -4,8 +4,13 @@ export class DbAccount implements AddAccount {
   constructor (private readonly hasher: Hasher, private readonly addAccountRepository: AddAccountRepository, private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
 
   async add (accountData: AccountDto): Promise<AccountModel> {
-    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
-    const hashedPassword = await this.hasher.hash(accountData.password)
-    return await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    // se o email passado pelo usuário não existir ele cria
+    // uma conta nova, se o email existir ele retorna null
+    const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      return await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    }
+    return null
   }
 }
